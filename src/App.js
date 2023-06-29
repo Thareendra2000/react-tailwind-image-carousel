dd4d0152f574c0387d15a8e7c2ba3e44ecb1f542
Thareendra2000 <thareendra2000@gmail.com>
2023-06-29 14:30:46 +0530

    Initialized the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import { BsArrowRightShort, BsArrowLeftShort } from 'react-icons/bs';
+import { RxDotFilled,RxDot } from 'react-icons/rx';
+import slides from './constatants/slides';
+
 
 function App() {
+  
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-[900px] h-[600px] w-full m-auto py-16 px-4 relative group">
+      <h1 className="text-3xl font-semi-bold mb-4 m-auto text-blue-900">React Image Slider using Tailwind CSS</h1>
+      <div
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+        className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
+      ></div>
+      {/* Left Arrow */}
+      <div className="hidden group-hover:block absolute top-[55%] -translate-x-0 translate-y-[-50%] left-0 text-2xl rounded-full p-2 bg-black/70 text-white cursor-pointer">
+        <BsArrowLeftShort onClick={prevSlide} size={30} />
+      </div>
+      {/* Right Arrow */}
+      <div className="hidden group-hover:block absolute top-[55%] -translate-x-0 translate-y-[-50%] right-0 text-2xl rounded-full p-2 bg-black/70 text-white cursor-pointer">
+        <BsArrowRightShort onClick={nextSlide} size={30} />
+      </div>
+      <div className="flex top-4 justify-center py-2">
+        {slides.map((slide, slideIndex) => (
+          <div
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+            className="text-2xl cursor-pointer"
+          >
+            {slideIndex === currentIndex ? (
+              <RxDotFilled className="mr-1 text-gray-700" />
+            ) : (
+              <RxDot className="mr-1 text-gray-700" />
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/constatants/slides.js
@@ -0,0 +1,24 @@
+const slides = [
+    {
+      url:
+        'https://images.unsplash.com/photo-1488998527040-85054a85150e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTJ8ODU2MDc5fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      url:
+        'https://images.unsplash.com/photo-1488998427799-e3362cec87c3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTF8ODU2MDc5fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      url:
+        'https://images.unsplash.com/photo-1456324463128-7ff6903988d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTN8ODU2MDc5fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      url:
+        'https://images.unsplash.com/photo-1554415707-6e8cfc93fe23?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8OHxUanNCaEUtWjFva3x8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      url:
+        'https://images.unsplash.com/photo-1601933513556-7926c45d1c49?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTR8VGpzQmhFLVoxb2t8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    },
+  ];
+
+  export default slides;
